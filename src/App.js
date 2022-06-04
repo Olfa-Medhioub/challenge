1ceb96e895f66531b8e093724d9412c7356856a8
Olfa-Medhioub <you@example.com>
2022-06-04 17:26:20 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import NavChallenge from './components/NavChallenge';
+import Profile from './components/Profile';
+import Projects from './components/Projects';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      < NavChallenge/>
+      <Profile/>
+      <Projects/>
+      <Contact/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Profile.js
@@ -0,0 +1,14 @@
+const Profile=()=>{
+    return(
+        <section id="about-me">
+        <h1>
+          Hello, my name is
+          <span className="rotate text-important">John doe</span>,<br />
+          and i make horrible websites.
+        </h1>
+        <img className="avatar" src="./John-Doe.jpg" alt="jhon-doe" />
+      </section>
+    )
+}
+
+export default Profile
